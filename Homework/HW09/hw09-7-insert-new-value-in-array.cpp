0f52e80fe0082fa05e40508e9257af07d76d6873
Muad2003 <anuphap2003118@gmail.com>
2023-09-14 12:27:20 +0700

    update homework 9-7

--- Homework/HW09/hw09-7-insert-new-value-in-array.cpp
+++ Homework/HW09/hw09-7-insert-new-value-in-array.cpp
@@ -16,3 +16,55 @@
         Old Array: 9 2 6 1 7
         New Array: 1 2 2 3 4 4 5 6 7 9
 */
+
+#include <stdio.h>
+
+int main() {
+    
+    int oldarray[ 5 ] = { 9 , 2 , 6 , 1 , 7 } ;
+    int n , temp ;
+
+    printf ( "Enter new element of Array :\n" ) ;
+    scanf ( "%d", &n ) ;
+
+    // สร้าง array ตัวใหม่ พร้อมรับค่า
+    int inputarray[ n ] ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        printf ( "Input :\n" ) ;
+        scanf ( "%d", &inputarray[ i ] ) ;
+    } // end for
+
+    // รวม array
+    int newarray[ n + 5 ] ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        newarray[ i ] = inputarray[ i ] ;
+    } // end for
+
+    for ( int i = n ; i < n + 5 ; i++ ) {
+        newarray[ i ] = oldarray[ i - n ] ;
+    } // end for
+
+    // เรียงจากเลขน้อยไปหามาก
+    for ( int i = 0 ; i < n + 5 ; i++ ) {
+        for ( int k = i + 1 ; k < n + 5 ; k++ ) {
+            if ( newarray[ i ] > newarray[ k ] ) {
+                temp = newarray[ i ] ;
+                newarray[ i ] = newarray[ k ] ;
+                newarray[ k ] = temp ;
+            } // end if
+        } // end for
+    } // end for
+    
+    // แสดงค่า
+    printf ( "Old Array:" ) ;
+    for ( int i = 0 ; i < 5 ; i++) {
+        printf ( " %d", oldarray[ i ] ) ;
+    } // end for
+    
+    printf ( "\nNew Array:" ) ;
+    for ( int i = 0 ; i < n + 5 ; i++ ) {
+        printf ( " %d", newarray[ i ] ) ;
+    } // end for
+    
+    return 0 ;
+}
